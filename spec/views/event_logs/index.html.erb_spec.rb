c739c5973ac2df06297a85c972e2d2f4a721b2b2
Arvind Kushwah <arvindkushwah9@gmail.com>
2019-09-17 20:05:52 +0530

    Fixed view specs

--- spec/views/event_logs/index.html.erb_spec.rb
+++ spec/views/event_logs/index.html.erb_spec.rb
@@ -3,20 +3,14 @@ require 'rails_helper'
 RSpec.describe "event_logs/index", type: :view do
   before(:each) do
     assign(:event_logs, [
-      EventLog.create!(
-        :type => "Type",
-        :desc => "Desc"
-      ),
-      EventLog.create!(
-        :type => "Type",
-        :desc => "Desc"
-      )
+      FactoryBot.create(:event_log),
+      FactoryBot.create(:event_log)
     ])
   end
 
   it "renders a list of event_logs" do
     render
-    assert_select "tr>td", :text => "Type".to_s, :count => 2
+    assert_select "tr>td", :text => "Subject".to_s, :count => 2
     assert_select "tr>td", :text => "Desc".to_s, :count => 2
   end
 end
--- spec/views/event_logs/show.html.erb_spec.rb
+++ spec/views/event_logs/show.html.erb_spec.rb
@@ -2,15 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "event_logs/show", type: :view do
   before(:each) do
-    @event_log = assign(:event_log, EventLog.create!(
-      :type => "Type",
-      :desc => "Desc"
-    ))
+    @event_log = assign(:event_log, FactoryBot.create(:event_log))
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Type/)
+    expect(rendered).to match(/Subject/)
     expect(rendered).to match(/Desc/)
   end
 end
