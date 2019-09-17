53187b25f3237a7f52121a603df0e0822d5af774
Arvind kushwah <arvindkushwah9@gmail.com>
2019-09-17 20:04:20 +0530

    club routing

---
+++ spec/routing/clubs_routing_spec.rb
@@ -0,0 +1,41 @@
+require "rails_helper"
+
+RSpec.describe UsersController, type: :routing do
+  describe "routing" do
+    it "routes to #index" do
+      expect(:get => "/clubs").to route_to("clubs#index")
+    end
+
+    it "routes to #new" do
+      expect(:get => "/clubs/new").to route_to("clubs#new")
+    end
+
+    it "routes to #show" do
+      expect(:get => "/clubs/1").to route_to("clubs#show", :id => "1")
+    end
+
+    it "routes to #edit" do
+      expect(:get => "/clubs/1/edit").to route_to("clubs#edit", :id => "1")
+    end
+
+    it "routes to #update via PUT" do
+      expect(:put => "/clubs/1").to route_to("clubs#update", :id => "1")
+    end
+
+    it "routes to #update via PATCH" do
+      expect(:patch => "/clubs/1").to route_to("clubs#update", :id => "1")
+    end
+
+    it "routes to #destroy" do
+      expect(:delete => "/clubs/1").to route_to("clubs#destroy", :id => "1")
+    end
+
+    it "routes to #admin" do
+      expect(:get => "/clubs/admin").to route_to("clubs#admin")
+    end
+
+    it "routes to #admin" do
+      expect(:get => "/clubs/admin").to route_to("clubs#admin")
+    end
+  end
+end
