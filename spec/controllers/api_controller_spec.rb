9fc3a1b3294414a9f436b913261e47899e91a59d
Arvind Kushwah <arvindkushwah9@gmail.com>
2019-09-17 19:46:27 +0530

    Fixed API controller specs

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -2,26 +2,27 @@ require 'rails_helper'
 
 RSpec.describe ApiController, type: :controller do
   login_user
-  describe "GET #upload_members" do
-    it "returns http success" do
-      get :upload_members
-      expect(response).to have_http_status(:success)
-    end
-  end
 
-  describe "GET #upload_awards" do
-    it "returns http success" do
-      get :upload_awards
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #import_members" do
+  #   it "returns http success" do
+  #     get :import_members
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET #upload_patrol_members" do
-    it "returns http success" do
-      get :upload_patrol_members
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #import_awards" do
+  #   it "returns http success" do
+  #     get :import_awards
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+
+  # describe "GET #import_patrol_members" do
+  #   it "returns http success" do
+  #     get :import_patrol_members
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
   # describe "GET #import_rosters" do
   #   it "returns http success" do
@@ -29,5 +30,27 @@ RSpec.describe ApiController, type: :controller do
   #     expect(response).to have_http_status(:success)
   #   end
   # end
+  let(:file_data) { Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, '/spec/fixtures/file.csv'))) }
+
+  describe "POST #upload_members" do
+    it "returns http success" do
+      post :upload_members, file_data: file_data
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST #upload_awards" do
+    it "returns http success" do
+      post :upload_awards, file_data: file_data
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+   describe "POST #upload_patrol_members" do
+    it "returns http success" do
+      post :upload_patrol_members, file_data: file_data
+      expect(response).to have_http_status(:success)
+    end
+  end
 
 end
