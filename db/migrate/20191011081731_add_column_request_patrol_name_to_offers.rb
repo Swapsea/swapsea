09f335caf3105b9e14859dbea3984c6abd988768
Arvind kushwah <arvindkushwah9@gmail.com>
2019-10-15 18:26:10 +0530

    Delete migration file

--- db/migrate/20191011081731_add_column_request_patrol_name_to_offers.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnRequestPatrolNameToOffers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :offers, :request_patrol_name, :string
-  end
-end
--- db/migrate/20191014063212_remove_column_from_offers.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromOffers < ActiveRecord::Migration[5.0]
-  def change
-  	remove_column :offers, :request_patrol_name
-  end
-end
