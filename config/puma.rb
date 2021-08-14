8ba5a71aeaf397fa55106ff7e9cc6a24063adcde
Mark Hudson <mark.hudson@ignitionadvice.com>
2021-08-14 15:56:34 +1000

    Remove old capistrano files

--- config/puma.rb
+++ config/puma.rb
@@ -11,7 +11,7 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # Worker specific setup for Rails 4.1+
+  # Worker specific setup for Rails 4.1+ but no longer needed for Rails 5.2+ apps
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
