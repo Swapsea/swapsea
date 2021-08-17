7ec030ac2735b1f898ce2df85057bfc50f923e33
Mark Hudson <mark.hudson@ignitionadvice.com>
2021-08-17 22:22:38 +1000

    Disable public_activity when running specs for performance.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'public_activity/testing'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
+
+# Don't write activity when running tests
+PublicActivity.enabled = false
+
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
