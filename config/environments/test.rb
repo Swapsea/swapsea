a05d8ac7727683e2567898c0393475d468d80da5
Mark Hudson <hellohuddo@gmail.com>
2021-08-27 22:48:13 +1000

    Update test environment to handle generated favicon

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,6 +18,8 @@ Swapsea2::Application.configure do
   config.serve_static_assets = true
   config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
 
+  config.assets.check_precompiled_asset = false
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
