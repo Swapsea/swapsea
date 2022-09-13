4bc9bad5a13d1609bc6b572b1ae8039221dccffe
Mark Hudson <hellohuddo@gmail.com>
2022-09-13 22:02:52 +1000

    Trigger send to all (active) Clubs

--- scheduled_task_send_email.sh
+++ scheduled_task_send_email.sh
@@ -4,16 +4,10 @@ if ((day == 3)); then
    echo "Yay, it's WEDNESDAY! Let's send those email reminders..."
 
    # SEND PATROL EMAILS
-   rails runner "Email.weekly_patrol_rosters('Avalon Beach SLSC')"
-   rails runner "Email.weekly_patrol_rosters('Bronte SLSC')"
-   rails runner "Email.weekly_patrol_rosters('North Bondi SLSC')"
-   rails runner "Email.weekly_patrol_rosters('Wanda SLSC Inc')"
-   rails runner "Email.weekly_patrol_rosters('Whale Beach SLSC Inc')"
+   rails runner "Email.weekly_patrol_rosters()"
 
    # SEND SKILLS MAINTENANCE EMAILS
-   rails runner "Email.weekly_skills_maintenance('Avalon Beach SLSC')"
-   rails runner "Email.weekly_skills_maintenance('North Bondi SLSC')"
-   rails runner "Email.weekly_skills_maintenance('Whale Beach SLSC Inc')"
+   rails runner "Email.weekly_skills_maintenance()"
 else
    echo "It's not Wednesday so nothing to do today."
 fi
