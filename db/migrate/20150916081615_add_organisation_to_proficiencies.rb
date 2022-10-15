0a0d57a1685c65948dcae1cdc3b9712c92bb5424
Mark Hudson <hellohuddo@gmail.com>
2022-10-15 21:48:11 +1100

    Prune old migrations earlier than 2022

--- db/migrate/20150916081615_add_organisation_to_proficiencies.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddOrganisationToProficiencies < ActiveRecord::Migration[5.0]
-  def change
-    add_column :proficiencies, :organisation, :string
-  end
-end
--- db/migrate/20181013235812_add_unique_constraint_to_award_number.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddUniqueConstraintToAwardNumber < ActiveRecord::Migration[5.0]
-  def change
-    add_index :awards, :award_number, unique: true
-  end
-end
