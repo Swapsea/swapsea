3975875735f2d4b9b4faea2b508d38fed8f0b53e
Mark Hudson <hellohuddo@gmail.com>
2022-11-16 21:14:01 +1100

    specs on new offer methods

--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -20,18 +20,38 @@ RSpec.describe Offer, type: :model do
 
   describe 'instance methods' do
     describe 'decline' do
-
-      it 'without remark' do
+      it 'status declined without remark' do
         @offer.decline(nil)
         expect(@offer.status).to eq('declined')
-        expect(@offer.decline_remark).to eq(nil)
+        expect(@offer.decline_remark).to be_nil
       end
 
-      it 'with remark' do
+      it 'status declined with remark' do
         remark = 'sorry mate'
         @offer.decline(remark)
         expect(@offer.status).to eq('declined')
         expect(@offer.decline_remark).to eq(remark)
+      end
+    end
+
+    describe 'withdraw' do
+      it 'status withdrawn' do
+        @offer.withdraw
+        expect(@offer.status).to eq('withdrawn')
+      end
+    end
+
+    describe 'cancel' do
+      it 'status cancelled' do
+        @offer.cancel
+        expect(@offer.status).to eq('cancelled')
+      end
+    end
+
+    describe 'unsuccessful' do
+      it 'status unsuccessful' do
+        @offer.unsuccessful
+        expect(@offer.status).to eq('unsuccessful')
       end
     end
   end
