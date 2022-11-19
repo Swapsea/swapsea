77a5b5fa0ed7cd0a73f809246a18c74b17e696c2
Mark Hudson <hellohuddo@gmail.com>
2022-11-20 01:27:13 +1100

    Screenshot failed tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,7 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'capybara-screenshot/rspec'
 require 'webmock/rspec'
 
 # Add additional requires below this line. Rails is not loaded until this point!
