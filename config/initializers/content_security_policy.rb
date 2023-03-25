26db8131d2c0574339c90d395479093f4a3195e7
Mark Hudson <hellohuddo@gmail.com>
2023-03-26 10:35:25 +1100

    Add CSP for New relic and Google Fonts

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,8 +6,8 @@ Rails.application.config.content_security_policy do |policy|
   policy.font_src    :self, :https, :data
   policy.img_src     :self, :https, :data
   policy.object_src  :none
-  policy.script_src  :self, :unsafe_inline, 'https://forecast.io', 'https://www.google-analytics.com', 'https://embed.doorbell.io'
-  policy.style_src   :self, :unsafe_inline, 'https://embed.doorbell.io'
+  policy.script_src  :self, :unsafe_inline, 'https://forecast.io', 'https://www.google-analytics.com', 'https://embed.doorbell.io', 'https://js-agent.newrelic.com'
+  policy.style_src   :self, :unsafe_inline, 'https://embed.doorbell.io', 'https://fonts.googleapis.com'
 end
 
 # Rails.application.config.content_security_policy_nonce_generator = -> request { request.session.id.to_s }
