3fead8a38294bd79083a4bf9ca70ac33c7024cdd
Mark Hudson <hellohuddo@gmail.com>
2023-08-06 20:55:28 +1000

    Remove unused JSON handlers

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -6,7 +6,6 @@ class ClubsController < ApplicationController
   layout 'dashboard'
 
   # GET /clubs
-  # GET /clubs.json
   def index
     render layout: 'admin'
   end
@@ -17,7 +16,6 @@ class ClubsController < ApplicationController
   end
 
   # GET /clubs/1
-  # GET /clubs/1.json
   def show
     render layout: 'admin'
   end
@@ -34,42 +32,34 @@ class ClubsController < ApplicationController
   end
 
   # POST /clubs
-  # POST /clubs.json
   def create
     @club = Club.new(club_params)
 
     respond_to do |format|
       if @club.save
         format.html { redirect_to @club, notice: 'Club was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @club }
       else
         format.html { render action: 'new' }
-        format.json { render json: @club.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /clubs/1
-  # PATCH/PUT /clubs/1.json
   def update
     respond_to do |format|
       if @club.update(club_params)
         format.html { redirect_to @club, notice: 'Club was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @club.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /clubs/1
-  # DELETE /clubs/1.json
   def destroy
     @club.destroy
     respond_to do |format|
       format.html { redirect_to clubs_url }
-      format.json { head :no_content }
     end
   end
 
